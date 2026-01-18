9b9bd6990c0eedfe8ada7614afea944b6b703b0a
ReallyLiri <reallyliri@gmail.com>
2026-01-18 12:09:00 +0100

    app

---
+++ app/.eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2021: true },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  settings: {
+    react: { version: 'detect' },
+  },
+}
