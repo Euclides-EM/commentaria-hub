ae97e47684f91ec859a2a7c826ccdb35d1145cd3
MiaMish <mia@cyolo.io>
2026-03-02 23:39:16 +0100

    push the wip ocr stuff

--- ocrflow/pkg/krakenwrapper/ocr.go
+++ ocrflow/pkg/krakenwrapper/ocr.go
@@ -10,6 +10,7 @@ import (
 	"sync"
 
 	"github.com/MiaMish/elements-dh/ocrflow/pkg/envexec"
+	"github.com/MiaMish/elements-dh/ocrflow/pkg/futils"
 )
 
 // RecognizeTextWithMapping overwrites existing ALTO files with OCR-ed ALTO output.
@@ -67,17 +68,45 @@ func validateImgAndAltoPaths(imgAndAltoPaths [][2]string) error {
 // Uses existing ALTO (segmentation+lines) as input and overwrites it with OCR-ed ALTO.
 func runPairsOCRUsingExistingAlto(imgAndAltoPaths [][2]string, ocrModel string) error {
 
+	// create temp dir that includes the ato and images
+	tmpDir, err := os.MkdirTemp("", "kraken_ocr_reuse_alto-*")
+	if err != nil {
+		return fmt.Errorf("could not create temp dir for kraken OCR reuse ALTO: %w", err)
+	}
+	defer os.RemoveAll(tmpDir)
+	tmpToOrigAltoPath := make(map[string]string) // tmp ALTO path -> original ALTO path
+	imgAndAltoTmpPaths := make([][2]string, 0)
+	for _, pair := range imgAndAltoPaths {
+		imgPath := pair[0]
+		altoPath := pair[1]
+
+		tmpImgPath := filepath.Join(tmpDir, filepath.Base(imgPath))
+		tmpAltoPath := filepath.Join(tmpDir, filepath.Base(altoPath))
+
+		if err := futils.CopyFile(imgPath, tmpImgPath); err != nil {
+			return fmt.Errorf("could not link image %s to temp location: %w", imgPath, err)
+		}
+		if err := futils.CopyFile(altoPath, tmpAltoPath); err != nil {
+			return fmt.Errorf("could not link ALTO %s to temp location: %w", altoPath, err)
+		}
+
+		imgAndAltoTmpPaths = append(imgAndAltoTmpPaths, [2]string{tmpImgPath, tmpAltoPath})
+		tmpToOrigAltoPath[tmpAltoPath] = altoPath
+	}
+
+	// Run Kraken OCR in parallel on pairs of (img, alto) files. Kraken will read the ALTO for segmentation
+
 	workers := runtime.NumCPU()
 	if workers > maxParallelKraken {
 		workers = maxParallelKraken
 	}
-	if workers > len(imgAndAltoPaths) {
-		workers = len(imgAndAltoPaths)
+	if workers > len(imgAndAltoTmpPaths) {
+		workers = len(imgAndAltoTmpPaths)
 	}
 	if workers < 1 {
 		workers = 1
 	}
-	chunks := chunkPairs(imgAndAltoPaths, workers)
+	chunks := chunkPairs(imgAndAltoTmpPaths, workers)
 
 	var mu sync.Mutex
 	var wg sync.WaitGroup
@@ -107,34 +136,33 @@ func runPairsOCRUsingExistingAlto(imgAndAltoPaths [][2]string, ocrModel string) 
 	var postMu sync.Mutex
 	var postErr error
 
-	postWg.Add(len(imgAndAltoPaths))
-	for _, pair := range imgAndAltoPaths {
-		altoPath := pair[1]
-		go func(final string) {
+	postWg.Add(len(imgAndAltoTmpPaths))
+	for _, pair := range imgAndAltoTmpPaths {
+		altoTmpPath := pair[1]
+		go func(altoTmpPath string) {
 			defer postWg.Done()
 
-			tmp := tmpOcredPath(final)
-
-			if err := RemovePathFromAltoImgFileName(tmp, tmp); err != nil {
+			tmp := tmpOcredPath(altoTmpPath)
+			if _, err := os.Stat(tmp); err != nil {
 				postMu.Lock()
 				if postErr == nil {
-					postErr = fmt.Errorf("could not fix ALTO file name in %s: %w", final, err)
+					postErr = fmt.Errorf("expected OCR output file %s does not exist: %w", tmp, err)
 				}
 				postMu.Unlock()
 				return
 			}
 
 			// Replace final with tmp (best effort atomic on same filesystem).
-			_ = os.Remove(final)
-			if err := os.Rename(tmp, final); err != nil {
+			_ = os.Remove(tmpToOrigAltoPath[altoTmpPath])
+			if err := os.Rename(tmp, tmpToOrigAltoPath[altoTmpPath]); err != nil {
 				postMu.Lock()
 				if postErr == nil {
-					postErr = fmt.Errorf("could not replace ALTO %s with OCR output: %w", final, err)
+					postErr = fmt.Errorf("could not replace ALTO %s with OCR output: %w", altoTmpPath, err)
 				}
 				postMu.Unlock()
 				return
 			}
-		}(altoPath)
+		}(altoTmpPath)
 	}
 	postWg.Wait()
 
@@ -142,13 +170,18 @@ func runPairsOCRUsingExistingAlto(imgAndAltoPaths [][2]string, ocrModel string) 
 }
 
 func runKrakenOCRReuseAlto(pairs [][2]string, ocrModel string) error {
+	// todo - this does not work!! Kraken CLI doesn't have the option to load existing ALTO for reuse.
+	//  The way to resolve it is to use a Python script - I started working on it but it is not ready yet.
+	//  See it in python-tools/ocr_for_segmented.py
+	//  Probably, much of my work to have absolute paths and temp files can be removed once we have the script...
+
 	if len(pairs) == 0 {
 		return nil
 	}
 
 	var args = []string{"--alto"}
 
-	// For each pair: -i <img> <existing_alto_in> <alto_out_tmp>
+	// For each pair: -i <img> <alto_out_tmp>
 	for _, p := range pairs {
 		imgPath := p[0]
 		altoOutTmp := tmpOcredPath(p[1])
@@ -161,7 +194,7 @@ func runKrakenOCRReuseAlto(pairs [][2]string, ocrModel string) error {
 		args = append(args, "-i", imgPath, altoOutTmp)
 	}
 
-	args = append(args, "ocr", "-m", ocrModel)
+	args = append(args, "segment", "-t", "alto", "ocr", "-m", ocrModel)
 
 	if err := envexec.PythonCmd("kraken", args...); err != nil {
 		return fmt.Errorf("kraken ocr (reuse alto) failed: %w", err)
