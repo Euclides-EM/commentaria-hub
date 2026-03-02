63bb8cd2adec3cf28b69eb6d56780727fc495202
ReallyLiri <reallyliri@gmail.com>
2026-03-02 21:15:20 +0100

    Updated generate api and app build

--- app/hub-api/hub-api/models/annotationrule_ModelDetect.ts
+++ app/hub-api/hub-api/models/annotationrule_ModelDetect.ts
@@ -5,7 +5,7 @@
 import type { annotationrule_PipelineStage } from './annotationrule_PipelineStage';
 import type { annotationrule_Type } from './annotationrule_Type';
 import type { common_OCRModelType } from './common_OCRModelType';
-export type annotationrule_Segment = {
+export type annotationrule_ModelDetect = {
     applicable_stages?: Array<annotationrule_PipelineStage>;
     model?: string;
     model_type?: common_OCRModelType;
