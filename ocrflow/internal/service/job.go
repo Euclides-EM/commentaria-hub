e57a51eb309cd04793c4819e2eb33d48f8307f2e
ReallyLiri <reallyliri@gmail.com>
2026-05-27 09:38:07 +0300

    wip

--- ocrflow/internal/service/job.go
+++ ocrflow/internal/service/job.go
@@ -77,7 +77,6 @@ func (j *Job) CreateJobs(ij *job.Jobs) (*job.Jobs, error) {
 
 func (j *Job) runAnnotationRuleApply(jb *job.Job) {
 	j.run(jb, "annotation rule apply", func() (any, error) {
-		time.Sleep(2 * time.Minute)
 		ann, err := j.annotations.ExecuteApplyRules(jb.Annotation.DatasetID, jb.Annotation.ID, jb.Rules)
 		if err != nil {
 			return nil, err
