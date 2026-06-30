7b779c3779d7dda198f754961f87f5e61d31cd54
ReallyLiri <reallyliri@gmail.com>
2026-06-30 13:10:47 +0300

    Re-gen models

---
+++ app/hub-api/hub-api/models/model_ReprintDetection.ts
@@ -0,0 +1,9 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { model_ReprintRelationship } from './model_ReprintRelationship';
+export type model_ReprintDetection = {
+    readonly candidates?: Array<model_ReprintRelationship>;
+};
+
