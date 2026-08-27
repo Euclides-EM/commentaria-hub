b747db5b98f9fe08a43aca6551eefa67088e8a5f
MiaMish <mia@cyolo.io>
2026-08-27 20:31:37 +0300

    Add types for annotation transcription fallback, format, and level

---
+++ app/hub-api/hub-api/models/annotation_TranscriptionFallback.ts
@@ -0,0 +1,11 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { annotation_TranscriptionFormat } from './annotation_TranscriptionFormat';
+import type { annotation_TranscriptionLevel } from './annotation_TranscriptionLevel';
+export type annotation_TranscriptionFallback = {
+    format: annotation_TranscriptionFormat;
+    level: annotation_TranscriptionLevel;
+    partial: boolean;
+};
